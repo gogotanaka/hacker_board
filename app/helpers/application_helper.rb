5e1ffb6afb28f4ab5c1d9ad1c27b76643328e87f
gogotanaka <qlli.illb@gmail.com>
2014-07-26 23:28:57 -0700

    Fix something

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def formalize(time)
     time.strftime("%Y-%m-%d %H:%M:%S")
   end
+
+  def news_title(news)
+    link_to news_jump_path(news), class: 'main-color', target: '_blank' do
+      truncate(news.title, length: 25)
+    end
+  end
 end
