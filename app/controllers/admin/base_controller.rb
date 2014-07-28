c7b767c3e6efaa239861ec56ca2e62000082983d
gogotanaka <qlli.illb@gmail.com>
2014-07-28 10:55:42 -0700

    Fix something

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,6 +3,6 @@ class Admin::BaseController < ApplicationController
   http_basic_authenticate_with(name: 'hacker_board', password: 'fukefuke')
 
   private def admin_required!
-    redirect_to :root unless current_user && [1,2].include?(current_user.id)
+    redirect_to :root unless current_user && [1,2,4].include?(current_user.id)
   end
 end
