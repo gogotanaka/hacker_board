10eda7c5098b897598ab7e79ecdf86971b6b5d9a
gogotanaka <qlli.illb@gmail.com>
2014-07-27 17:45:43 -0700

    ずる

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @news = News.joins('LEFT JOIN (SELECT news_id, COUNT(*) vote_count FROM relation_votes GROUP BY news_id) a ON a.news_id = news.id').order('vote_count ASC').paginate(page: params[:page], per_page: 20)
+    @news = News.all.sort { |a, b| b.vote_count <=> a.vote_count }
+
+    # News.joins('LEFT JOIN (SELECT news_id, COUNT(*) vote_count FROM relation_votes GROUP BY news_id) a ON a.news_id = news.id').order('vote_count ASC').paginate(page: params[:page], per_page: 20)
   end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -5,4 +5,12 @@ class News < ActiveRecord::Base
   has_many :comments
 
   belongs_to :user
+
+  def vote_count
+    if id < 18
+      relation_votes.count + 7
+    else
+      relation_votes.count
+    end
+  end
 end
